0d5281a2c54f5710f18ae4c7ee157062d7b573f3
apola121 <sellerinus@gmail.com>
2022-03-25 23:28:22 -0400

    update7

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "Hooks"},
-        tags = "@SmokeTestAPI",
+        tags = "@SmokeTestDB",
         dryRun = false
 )
 public class Runner {
